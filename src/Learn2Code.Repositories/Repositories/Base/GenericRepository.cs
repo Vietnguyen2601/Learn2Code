8e5ab3bd8e90ab0003e70da6debb4a36bd57822e
DressAnDy <mnhat2232004@gmail.com>
2026-02-03 08:27:48 +0700

    Push CodeBase

---
+++ src/Learn2Code.Repositories/Repositories/Base/GenericRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learn2Code.Infrastructure.Repositories.Base
+{
+    internal class GenericRepository
+    {
+    }
+}
---
+++ src/Learn2Code.Services/Base/IServiceResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learn2Code.Application.Base
+{
+    public class IServiceResult
+    {
+
+    }
+}
