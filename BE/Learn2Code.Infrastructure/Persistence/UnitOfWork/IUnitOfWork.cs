cfb540528820a122d2cd55e7907c46af9f020755
DressAnDy <mnhat2232004@gmail.com>
2026-02-04 21:35:01 +0700

    Pushing CodeBase ( Generic + UnitOfWork + ServiceResult)

--- BE/Learn2Code.Infrastructure/Persistence/UnitOfWork/IUnitOfWork.cs
+++ BE/Learn2Code.Infrastructure/Persistence/UnitOfWork/IUnitOfWork.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Learn2Code.Infrastructure.Persistence.UnitOfWork;
 
-namespace Learn2Code.Infrastructure.Persistence.UnitOfWork
+public interface IUnitOfWork : IDisposable, IAsyncDisposable
 {
-    internal class IUnitOfWork
-    {
-    }
+    /// Get repository for entity type T
+    // IGenericRepository<T> Repository<T>() where T : class;
+    /// Save all pending changes synchronously
+    // int SaveChanges();
+    // /// Save all pending changes asynchronously
+    // Task<int> SaveChangesAsync();
+    // /// Begin a database transaction
+    // Task BeginTransactionAsync();
+    // /// Commit the current transaction
+    // Task CommitTransactionAsync();
+    // /// Rollback the current transaction
+    // Task RollbackTransactionAsync();
 }
--- BE/Learn2Code.Infrastructure/Persistence/UnitOfWork/UnitOfWork.cs
+++ BE/Learn2Code.Infrastructure/Persistence/UnitOfWork/UnitOfWork.cs
@@ -1,12 +1,107 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+// using Learn2Code.Infrastructure.Data.Context;
+using Learn2Code.Infrastructure.Repositories.Base;
 
-namespace Learn2Code.Infrastructure.Persistence.UnitOfWork
+namespace Learn2Code.Infrastructure.Persistence.UnitOfWork;
+
+public class UnitOfWork
 {
-    internal class UnitOfWork
-    {
-    }
+    // private readonly ApplicationDbContext _context;
+    // private IDbContextTransaction _transaction;
+    // private readonly Dictionary<string, object> _repositories;
+
+    // public UnitOfWork(ApplicationDbContext context)
+    // {
+    //     _context = context;
+    //     _repositories = new Dictionary<string, object>();
+    // }
+
+    // /// Get or create a repository for entity type T
+    // public IGenericRepository<T> Repository<T>() where T : class
+    // {
+    //     var key = typeof(T).Name;
+
+    //     if (!_repositories.ContainsKey(key))
+    //     {
+    //         var repositoryType = typeof(GenericRepository<>).MakeGenericType(typeof(T));
+    //         var repositoryInstance = Activator.CreateInstance(repositoryType, _context);
+    //         _repositories.Add(key, repositoryInstance);
+    //     }
+
+    //     return (IGenericRepository<T>)_repositories[key];
+    // }
+
+    // /// Save all pending changes synchronously
+    // public int SaveChanges()
+    // {
+    //     return _context.SaveChanges();
+    // }
+
+    // /// Save all pending changes asynchronously
+    // public async Task<int> SaveChangesAsync()
+    // {
+    //     return await _context.SaveChangesAsync();
+    // }
+
+    // /// Begin a database transaction
+    // public async Task BeginTransactionAsync()
+    // {
+    //     _transaction = await _context.Database.BeginTransactionAsync();
+    // }
+
+    // /// Commit the current transaction
+    // public async Task CommitTransactionAsync()
+    // {
+    //     try
+    //     {
+    //         await SaveChangesAsync();
+    //         await _transaction?.CommitAsync();
+    //     }
+    //     catch
+    //     {
+    //         await RollbackTransactionAsync();
+    //         throw;
+    //     }
+    //     finally
+    //     {
+    //         // await _transaction?.DisposeAsync();
+    //         _transaction = null;
+    //     }
+    // }
+
+    // /// Rollback the current transaction
+    // public async Task RollbackTransactionAsync()
+    // {
+    //     try
+    //     {
+    //         await _transaction?.RollbackAsync();
+    //     }
+    //     finally
+    //     {
+    //         // await _transaction?.DisposeAsync();
+    //         _transaction = null;
+    //     }
+    // }
+
+    // /// Dispose resources
+    // public void Dispose()
+    // {
+    //     _transaction?.Dispose();
+    //     _context?.Dispose();
+    // }
+
+    // /// Dispose resources asynchronously
+    // public async ValueTask DisposeAsync()
+    // {
+    //     if (_transaction != null)
+    //     {
+    //         await _transaction.DisposeAsync();
+    //     }
+
+    //     if (_context != null)
+    //     {
+    //         await _context.DisposeAsync();
+    //     }
+    // }
 }
